c46522aae25a2f3eab3b07021df318a91f38f274
stilet00 <safroninanton@gmail.com>
2021-01-07 00:10:46 +0200

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
     context: path.join(__dirname),
-    mode: "development",
+    mode: "production",
     module: {
         rules: [
             {
