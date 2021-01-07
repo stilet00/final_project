68349153684bf923abc51907b7ec2839a759b592
stilet00 <safroninanton@gmail.com>
2021-01-07 11:31:35 +0200

    adding loaders

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,7 @@
 import View from './view';
 import Model from './model';
 import Controller from './controller'
+import '../styles/style.css';
 document.addEventListener('DOMContentLoaded', () => {
 let view = new View();
 let model = new Model(view);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    context: path.join(__dirname),
+    context: path.resolve(__dirname, 'src'),
     mode: "production",
     module: {
         rules: [
@@ -13,12 +14,23 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+
             }
         ]
     },
-    entry: "/src/scripts/script.js",
+
+    entry: "/scripts/script.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        })
+    ]
 }
